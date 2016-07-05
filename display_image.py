ef5029410ead748200bf699e046e0cb4f0594476
Satwik Mishra <sumumishra@gmail.com>
2016-07-05 20:22:24 +0530

    image file display

---
+++ display_image.py
@@ -0,0 +1,11 @@
+import gzip
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+import pickle
+
+
+with gzip.open('C:\Program Files\Sublime Text 3\mnist.pkl.gz', 'rb') as f:
+    train_set, valid_set, test_set = pickle.load(f)
+train_x, train_y = train_set
+plt.imshow(train_x[0].reshape((28, 28)), cmap = cm.Greys_r)
+plt.show()
